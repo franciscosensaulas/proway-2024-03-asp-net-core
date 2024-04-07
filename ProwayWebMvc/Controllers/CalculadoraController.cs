13bb027be30ab0372236c078aa6a7e68714e9019
Francisco Lucas Sens <franciscosensaulas@gmail.com>
2024-04-07 18:53:40 -0300

    Adicionado gerar excel local

--- ProwayWebMvc/Controllers/CalculadoraController.cs
+++ ProwayWebMvc/Controllers/CalculadoraController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Data;
 
 namespace ProwayWebMvc.Controllers
 {
@@ -9,6 +13,51 @@ namespace ProwayWebMvc.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            List<UserDetails> persons = new List<UserDetails>()
+            {
+                new UserDetails() {ID="1001", Name="ABCD", City ="City1", Country="USA"},
+                new UserDetails() {ID="1002", Name="PQRS", City ="City2", Country="INDIA"},
+                new UserDetails() {ID="1003", Name="XYZZ", City ="City3", Country="CHINA"},
+                new UserDetails() {ID="1004", Name="LMNO", City ="City4", Country="UK"},
+           };
+
+            // Lets converts our object data to Datatable for a simplified logic.
+            // Datatable is most easy way to deal with complex datatypes for easy reading and formatting.
+
+            DataTable table = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(persons), (typeof(DataTable)));
+            var memoryStream = new MemoryStream();
+
+            using (var fs = new FileStream("Result.xlsx", FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet excelSheet = workbook.CreateSheet("Sheet1");
+
+                List<String> columns = new List<string>();
+                IRow row = excelSheet.CreateRow(0);
+                int columnIndex = 0;
+
+                foreach (System.Data.DataColumn column in table.Columns)
+                {
+                    columns.Add(column.ColumnName);
+                    row.CreateCell(columnIndex).SetCellValue(column.ColumnName);
+                    columnIndex++;
+                }
+
+                int rowIndex = 1;
+                foreach (DataRow dsrow in table.Rows)
+                {
+                    row = excelSheet.CreateRow(rowIndex);
+                    int cellIndex = 0;
+                    foreach (String col in columns)
+                    {
+                        row.CreateCell(cellIndex).SetCellValue(dsrow[col].ToString());
+                        cellIndex++;
+                    }
+
+                    rowIndex++;
+                }
+                workbook.Write(fs);
+            }
             return View();
         }
 
---
+++ ProwayWebMvc/Controllers/UserDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace ProwayWebMvc.Controllers
+{
+    internal class UserDetails
+    {
+        public UserDetails()
+        {
+        }
+
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
